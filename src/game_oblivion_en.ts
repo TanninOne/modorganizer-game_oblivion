c289b5f02f7c0f7a36e0a7dc421146d2e24c6ef0
Tannin <sherb@gmx.net>
2016-06-04 20:33:46 +0200

    translation

---
+++ src/game_oblivion_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>GameOblivion</name>
+    <message>
+        <location filename="gameoblivion.cpp" line="69"/>
+        <source>Adds support for the game Oblivion</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
